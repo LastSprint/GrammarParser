178a9a7a7aa31c492c7677f03e4224ff2f72de34
Александр Кравченков <LastSprint@yandex.ru>
2018-02-23 20:25:20 +0300

    Добавил файлы. Зачем-то.

---
+++ GrammarParser/GrammarParser/Lexer/Parser/Classes/OneOrZeroParser.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GrammarParser.Lexer.Parser.Interfaces;
+
+namespace GrammarParser.Lexer.Parser.Classes {
+
+}
---
+++ GrammarParser/GrammarParser/Lexer/Parser/Classes/SingleRuleParser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrammarParser.Lexer.Parser.Classes
+{
+    class SingleRuleParser
+    {
+    }
+}
